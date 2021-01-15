7aab6af1b8eb5444e8c94db2ac09fe4a12cba44c
Mike Rivera <michaelstephenrivera@gmail.com>
2021-01-15 15:37:04 -0600

    Admin Update User functionality: Backend - Created route/controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,9 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -20,5 +22,7 @@ router.route('/profile')
   .put(protect, updateUserProfile)
 router.route('/:id')
   .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
