cef60cd123a18946eb2498af4bf9ddbf0da86c58
Mike Rivera <michaelstephenrivera@gmail.com>
2021-01-19 13:30:47 -0600

    Admin Product Update/Delete functionality: Backend - controller/routes created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router()
 import {
   getProducts,
   getProductById,
-  deleteProduct
+  deleteProduct,
+  createProduct,
+  updateProduct
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -11,9 +13,11 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/')
   .get(getProducts)
+  .post(protect, admin, createProduct)
 
 router.route('/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
